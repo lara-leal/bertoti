b76aefc342a3e0d841f5bca74448aa6c9363d4f1
Lara Leal <80706297+lara-leal@users.noreply.github.com>
2022-11-30 09:02:05 -0300

    Update TabuleiroXadrez.java

--- PadroesdeProjeto/Singleton/src/pattern/TabuleiroXadrez.java
+++ PadroesdeProjeto/Singleton/src/pattern/TabuleiroXadrez.java
@@ -5,19 +5,11 @@ public class TabuleiroXadrez {
     static private TabuleiroXadrez instance = null;
     
     private TabuleiroXadrez(){}
+    
     static TabuleiroXadrez getInstance()
     {
     	if( instance == null)
            instance = new TabuleiroXadrez();
         return instance;
-    }
-    
-    public static void main (String args[]) 
-    {
-    	TabuleiroXadrez tabuleiro1 = TabuleiroXadrez.getInstance();
-    	TabuleiroXadrez tabuleiro2 = TabuleiroXadrez.getInstance();
-    	System.out.println(tabuleiro1);
-    	System.out.println(tabuleiro2);
-    }
-    
-    }
+    }  
+}
